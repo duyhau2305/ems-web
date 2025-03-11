5994aeb2bf73ac054d028074ca583ec7032a4b41
unknown <duyhau2305@gmail.com>
2025-03-11 08:10:53 +0700

    asd

--- datainsight/vite.config.js
+++ datainsight/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: CONFIG.base || '/',
   plugins: [react()],
   base: "/vite-dataInsight/"
 })
